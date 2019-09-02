df5f4ae6d11e7e34fd2bea4965b07bf402603183
xiaoyanh <xiaoyanhn@gmail.com>
2019-09-02 11:34:55 -0400

    optimization data

---
+++ algs/opt_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 10:18:22 2019
+
+@author: Xiaoyan
+"""
+# Defines an objective
+# Calls the oracle
+
+class OptData:
+    def __init__(self, max_iter = 1000):
+        self.max_iter   = 1000
+        self.total_iter = 0
+        self.opt_fx    = None
+        self.opt_x     = None
+        self.path_x    = None
+        self.path_fx   = None
+    
+    def optimize(x0):
+        
+        stop = False
+        
+        for i in range(self.max_iter):
+            
+            
+            self.total_iter = break
+            stop = 
+            
+            if stop:
+                break
