441c9f1558453d7e08e6c322ab597f60b56e1676
Xiaoyan <xiaoyanh@gmail.com>
2020-05-14 10:50:02 -0400

    created strongly convex function gen

--- obj/obj_funcs.py
+++ obj/obj_funcs.py
@@ -1,4 +1,5 @@
 import torch
+from torch import max, norm, stack, Tensor, tensor
 from obj.objective import Objective
 
 def simple2D(x):
@@ -6,22 +7,22 @@ def simple2D(x):
 Simple2D = Objective(simple2D)
 
 # Creates a strongly convex objective function for particular n and k
-def stronglyconvex(n=50, k=10, seed=0):
+def stronglyconvex(n=50, k=10, seed=0, **kwargs):
     torch.random.manual_seed(seed)
 
     c = torch.randn(k)
     g = torch.randn(k,n)
     tmp = torch.randn(k,n,n)
-    H = torch.stack([tmp[i,:,:].T @ tmp[i,:,:] for i in range(k)])
+    H = stack([tmp[i,:,:].T @ tmp[i,:,:] for i in range(k)])
 
     def sc_function(x):
-        if type(x) != torch.Tensor: # If non-tensor passed in, no gradient will be used
-            x = torch.tensor(x, dtype=torch.float, requires_grad=False)
+        if type(x) != Tensor: # If non-tensor passed in, no gradient will be used
+            x = tensor(x, dtype=torch.float, requires_grad=False)
         assert len(x) == n
 
         term1 = g@x
-        term2 = 0.5 * torch.stack([x.T @ H[i,:,:] @ x for i in range(k)])
-        term3 = (1./24) * (torch.norm(x)**4) * c
-        return torch.max(term1+term2+term3)
+        term2 = 0.5 * stack([x.T @ H[i,:,:] @ x for i in range(k)])
+        term3 = (1./24.) * (norm(x)**4) * c
+        return max(term1+term2+term3)
 
-    return Objective(sc_function)
+    return Objective(sc_function, **kwargs)
